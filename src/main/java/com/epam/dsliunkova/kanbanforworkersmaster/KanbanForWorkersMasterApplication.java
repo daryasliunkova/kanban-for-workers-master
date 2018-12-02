eb8d9d2b953f30bfd8e32fd012cdb3b5f05a7694
Dasha <daryasliunkova@gmail.com>
2018-12-02 23:02:52 +0300

    initial commit

---
+++ src/main/java/com/epam/dsliunkova/kanbanforworkersmaster/KanbanForWorkersMasterApplication.java
@@ -0,0 +1,12 @@
+package com.epam.dsliunkova.kanbanforworkersmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanForWorkersMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanForWorkersMasterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/epam/dsliunkova/kanbanforworkersmaster/KanbanForWorkersMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epam.dsliunkova.kanbanforworkersmaster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KanbanForWorkersMasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
